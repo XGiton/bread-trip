469666e459b8f2a2ee2cf3002aadcc8f7d2990e0
肖志栋 <xgiton@qq.com>
2017-10-26 21:28:41 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="home">
+    <ul class="trips fn-clear">
+      <li v-for="trip in trips">
+        <a href="#" target="_blank" class="cover" :title="trip.title" :style="'background-image: url(' + trip.cover_url + ')'"></a>
+        <a href="#" :title="trip.title" target="_blank" class="name">{{ trip.title }}</a>
+        <div class="info">
+          <a href="#" target="_blank" class="user" :title="trip.user.name">
+            <img :src="trip.user.avatar_url" alt>
+          </a>
+          <p class="summary">
+            <span>{{ trip.time }}</span>
+            <span>{{ trip.days }}天</span>
+            <span>{{ trip.view_count }}次浏览</span>
+          </p>
+          <p class="location">{{ trip.location }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { getHomeData } from '../modules/home'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      trips: []
+    }
+  },
+
+  created () {
+    this.trips = getHomeData()
+  }
+}
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  .home {
+    margin-top: 50px;
+  }
+
+  .fn-clear {
+    zoom: 1;
+  }
+
+  .trips li {
+    width: 285px;
+    float: left;
+    border-radius: 4px;
+    overflow: hidden;
+    background-color: #fff;
+    margin: 0 0 20px 20px;
+  }
+
+  .trips li .cover {
+    display: block;
+    height: 260px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  .trips li .name {
+    display: block;
+    height: 20px;
+    font-size: 18px;
+    line-height: 20px;
+    color: #5c5c5c;
+    padding: 0 20px 0 16px;
+    margin-top: 18px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  a, a:link {
+    text-decoration: none;
+    transition: color .3s ease;
+  }
+
+  .trips li .info {
+    padding: 0 16px 16px 14px;
+    margin: 20px 0 0;
+    height: 38px;
+    color: #999;
+    font-size: 12px;
+    overflow: hidden;
+    vertical-align: baseline;
+  }
+
+  .trips li  .user {
+    float: right;
+  }
+
+  .trips li .user img {
+    width: 38px;
+    vertical-align: bottom;
+    border-radius: 38px;
+    text-decoration: none;
+  }
+
+  .trips li .summary {
+    margin-bottom: 12px;
+  }
+
+  .trips li .summary span {
+    margin-left: 12px;
+  }
+
+  .trips li .location {
+
+  }
+</style>
---
+++ src/modules/home.js
@@ -0,0 +1,80 @@
+
+function getHomeData () {
+  return [
+    {
+      cover_url: 'http://photos.breadtrip.com/covers_2017_10_26_256c3c6063d587c9856e8fcf797b9e4f.jpg?imageView/2/w/960/',
+      title: '日本: 宇奈月温泉，立山黑部',
+      time: '2017.10.03',
+      days: 6,
+      view_count: 7736,
+      location: '日本, 黑部市',
+      user: {
+        name: '黑色键盘',
+        avatar_url: 'http://photos.breadtrip.com/avatar_56_a1_2e9298a132e4e417d4a89e0c221f5d50803d4cab.jpg-avatar.m'
+      }
+    },
+    {
+      cover_url: 'http://photos.breadtrip.com/covers_2017_10_26_59629d8dc9b0775f8a733ddd58622fc2.jpg?imageView/2/w/960/',
+      title: 'Europe 遇见无与伦比的美丽🇩🇪🇨🇭🇫🇷',
+      time: '2017.10.03',
+      days: 13,
+      view_count: 27370,
+      location: '瑞士,Ennetbürgen',
+      user: {
+        name: '黑色键盘',
+        avatar_url: 'http://photos.breadtrip.com/avatar_56_a1_2e9298a132e4e417d4a89e0c221f5d50803d4cab.jpg-avatar.m'
+      }
+    },
+    {
+      cover_url: 'http://photos.breadtrip.com/covers_2017_10_25_54a8c10570975239d44487df4c59499e.jpg?imageView/2/w/960/',
+      title: '松格嘛呢石经城 这里的石头会唱歌',
+      time: '2017.10.03',
+      days: 6,
+      view_count: 15272,
+      location: '中国,甘孜藏族自治州',
+      user: {
+        name: '黑色键盘',
+        avatar_url: 'http://photos.breadtrip.com/avatar_56_a1_2e9298a132e4e417d4a89e0c221f5d50803d4cab.jpg-avatar.m'
+      }
+    },
+    {
+      cover_url: 'http://photos.breadtrip.com/covers_2017_10_26_256c3c6063d587c9856e8fcf797b9e4f.jpg?imageView/2/w/960/',
+      title: '日本: 宇奈月温泉，立山黑部',
+      time: '2017.10.03',
+      days: 6,
+      view_count: 7736,
+      location: '日本, 黑部市',
+      user: {
+        name: '黑色键盘',
+        avatar_url: 'http://photos.breadtrip.com/avatar_56_a1_2e9298a132e4e417d4a89e0c221f5d50803d4cab.jpg-avatar.m'
+      }
+    },
+    {
+      cover_url: 'http://photos.breadtrip.com/covers_2017_10_26_256c3c6063d587c9856e8fcf797b9e4f.jpg?imageView/2/w/960/',
+      title: '日本: 宇奈月温泉，立山黑部',
+      time: '2017.10.03',
+      days: 6,
+      view_count: 7736,
+      location: '日本, 黑部市',
+      user: {
+        name: '黑色键盘',
+        avatar_url: 'http://photos.breadtrip.com/avatar_56_a1_2e9298a132e4e417d4a89e0c221f5d50803d4cab.jpg-avatar.m'
+      }
+    },
+    {
+      cover_url: 'http://photos.breadtrip.com/covers_2017_10_26_256c3c6063d587c9856e8fcf797b9e4f.jpg?imageView/2/w/960/',
+      title: '日本: 宇奈月温泉，立山黑部',
+      time: '2017.10.03',
+      days: 6,
+      view_count: 7736,
+      location: '日本, 黑部市',
+      user: {
+        name: '黑色键盘',
+        avatar_url: 'http://photos.breadtrip.com/avatar_56_a1_2e9298a132e4e417d4a89e0c221f5d50803d4cab.jpg-avatar.m'
+      }
+    }
+  ]
+}
+export {
+  getHomeData
+}
